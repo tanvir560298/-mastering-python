229f72d0b8166aa1f2e90ea09435d0f378daa2a8
tanvir560298 <tahmadium@gmail.com>
2025-06-21 14:39:15 -0700

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+# This is a simple calculator made with python
+print("This calculator can do operations between 2 operands")
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("The Addition of " + a + " and " + b + " is: " + str(int(a) + int(b)))
+print("The Subtraction of " + a + " and " + b + " is: " + str(int(a) - int(b)))
+print("The Multiplication of " + a + " and " + b + " is: " + str(int(a) * int(b)))
+print("The Division of " + a + " and " + b + " is: " + str(int(a) / int(b)))
+print("The Modulus of " + a + " and " + b + " is: " + str(int(a) % int(b)))
+print("The Exponentiation of " + a + " and " + b + " is: " + str(int(a) ** int(b)))
+
+
+
+
+
